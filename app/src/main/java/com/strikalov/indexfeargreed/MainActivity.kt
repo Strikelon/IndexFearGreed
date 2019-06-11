7f06f50ca9c7cc76da7b9d5e9585109742877556
Alexander Strikalov <strikalov.aleksandr@yandex.ru>
2019-06-11 16:00:21 +0300

    add CustomView

--- app/src/main/java/com/strikalov/indexfeargreed/MainActivity.kt
+++ app/src/main/java/com/strikalov/indexfeargreed/MainActivity.kt
@@ -11,6 +11,13 @@ import com.strikalov.indexfeargreed.model.repositories.NetworkRepository
 import com.strikalov.indexfeargreed.model.repositories.NetworkRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.internal.util.HalfSerializer.onNext
+import kotlinx.android.synthetic.main.activity_main.*
+import android.animation.AnimatorSet
+import android.view.animation.DecelerateInterpolator
+import android.animation.ObjectAnimator
+import android.view.animation.AccelerateInterpolator
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,28 +25,38 @@ class MainActivity : AppCompatActivity() {
         private const val TAG_LOG = "MainActivityLog"
     }
 
-    private lateinit var webApi: WebApi
-    private lateinit var networkRepository: NetworkRepository
-    private lateinit var fearGreedIndexInteractor: FearGreedIndexInteractor
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webApi = WebApi()
-        networkRepository = NetworkRepositoryImpl(webApi)
-        fearGreedIndexInteractor = FearGreedIndexInteractorImpl(networkRepository)
+        custom_view.setOnClickListener {
+            startAnimation()
+        }
 
-        fearGreedIndexInteractor.loadFearGreedIndexList(7)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
-                    Log.i(TAG_LOG, it.fearGreedIndexList.toString())
-                },
-                {
-                    Log.i(TAG_LOG, it.toString())
-                }
-            )
+    }
+
+    private fun startAnimation() {
+
+        val random = Random()
+        var index = 0
+        index = random.nextInt(100)
+        Log.i("MyLogger", "index = $index")
+
+        val animatorFirst = ObjectAnimator
+            .ofInt(custom_view, "indexFearGreedValue", 0, 100)
+            .setDuration(1500)
+        animatorFirst.interpolator = AccelerateInterpolator()
+
+        val animatorSecond = ObjectAnimator
+            .ofInt(custom_view, "indexFearGreedValue", 100, index)
+            .setDuration(1000)
+        animatorSecond.interpolator = DecelerateInterpolator()
+
+        val animatorSet = AnimatorSet()
+        animatorSet
+            .play(animatorFirst)
+            .before(animatorSecond)
+        animatorSet.start()
 
     }
 }
---
+++ app/src/main/java/com/strikalov/indexfeargreed/view/CustomView.kt
@@ -0,0 +1,162 @@
+package com.strikalov.indexfeargreed.view
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.strikalov.indexfeargreed.R
+import android.graphics.LinearGradient
+
+class CustomView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    companion object {
+        private const val DEFAULT_FIRST_ARC_COLOR = Color.RED
+        private const val DEFAULT_SECOND_ARC_COLOR = Color.YELLOW
+        private const val DEFAULT_THIRD_ARC_COLOR = Color.GREEN
+        private const val DEFAULT_ARROW_COLOR = Color.BLACK
+        private const val DEFAULT_ARROW_TEXT_COLOR = Color.WHITE
+        private const val DEFAULT_ARC_WIDTH = 50f
+        private const val DEFAULT_ARROW_RADIUS = 50f
+        private const val DEFAULT_ARROW_LENGTH = 200f
+        private const val DEFAULT_ARROW_WIDTH = 15f
+        private const val DEFAULT_INDEX_FEAR_GREED_VALUE = 0
+        private const val DEFAULT_ARC_ANGEL = 180f
+        private const val DEFAULT_FONT_SIZE = 55f
+    }
+
+    private val arcPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val arrowPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val fontPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val arrowPath: Path = Path()
+    private val arrowMatrix: Matrix = Matrix()
+    private val oval: RectF = RectF()
+
+    private var firstArcColor: Int = DEFAULT_FIRST_ARC_COLOR
+    private var secondArcColor: Int = DEFAULT_SECOND_ARC_COLOR
+    private var thirdArcColor: Int = DEFAULT_THIRD_ARC_COLOR
+    private var arrowColor: Int = DEFAULT_ARROW_COLOR
+    private var arrowTextColor: Int = DEFAULT_ARROW_TEXT_COLOR
+    private var arcWidth: Float = DEFAULT_ARC_WIDTH
+    private var arrowRadius: Float = DEFAULT_ARROW_RADIUS
+    private var arrowLength: Float = DEFAULT_ARROW_LENGTH
+    private var arrowWidth: Float = DEFAULT_ARROW_WIDTH
+    private var fontSize: Float = DEFAULT_FONT_SIZE
+
+    private var indexFearGreedValue: Int = DEFAULT_INDEX_FEAR_GREED_VALUE
+        set(value){
+            field = value
+            invalidate()
+        }
+
+    private var radius: Float = 0f
+    private var centerX: Float = 0f
+    private var centerY: Float = 0f
+
+    init{
+        setupAttrs(context, attrs)
+    }
+
+    private fun setupAttrs(context:Context, attrs:AttributeSet){
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomView, 0, 0)
+
+        try {
+
+            firstArcColor = typedArray.getColor(R.styleable.CustomView_firstArcColor, DEFAULT_FIRST_ARC_COLOR)
+            secondArcColor = typedArray.getColor(R.styleable.CustomView_secondArcColor, DEFAULT_SECOND_ARC_COLOR)
+            thirdArcColor = typedArray.getColor(R.styleable.CustomView_thirdArcColor, DEFAULT_THIRD_ARC_COLOR)
+            arcWidth = typedArray.getDimension(R.styleable.CustomView_arcWidth, DEFAULT_ARC_WIDTH)
+            arrowColor = typedArray.getColor(R.styleable.CustomView_arrowColor, DEFAULT_ARROW_COLOR)
+            arrowTextColor = typedArray.getColor(R.styleable.CustomView_arrowTextColor, DEFAULT_ARROW_TEXT_COLOR)
+            arrowRadius = typedArray.getDimension(R.styleable.CustomView_arrowRadius, DEFAULT_ARROW_RADIUS)
+            arrowLength = typedArray.getDimension(R.styleable.CustomView_arrowLength, DEFAULT_ARROW_LENGTH)
+            indexFearGreedValue = typedArray.getInt(R.styleable.CustomView_indexFearGreedValue, DEFAULT_INDEX_FEAR_GREED_VALUE)
+            arrowWidth = typedArray.getDimension(R.styleable.CustomView_arrowWidth, DEFAULT_ARROW_WIDTH)
+            fontSize = typedArray.getDimension(R.styleable.CustomView_fontSize, DEFAULT_FONT_SIZE)
+
+        } finally {
+            typedArray.recycle()
+        }
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    override fun onDrawForeground(canvas: Canvas?) {
+        super.onDrawForeground(canvas)
+        val width = width
+        val height = height
+
+        radius = (width / 2.5f)
+        centerX = width / 2f
+        centerY = height - (arrowRadius + arrowWidth) * 1.2f
+
+        drawArc(canvas)
+        drawArrow(canvas)
+    }
+
+    private fun drawArc(canvas: Canvas?) {
+
+        arcPaint.shader = LinearGradient(
+            centerX - radius, 0f, centerX + radius, 0f,
+            intArrayOf(firstArcColor, secondArcColor, thirdArcColor), floatArrayOf(0f, 0.5f, 1f), Shader.TileMode.CLAMP
+        )
+
+        arcPaint.strokeWidth = arcWidth
+        arcPaint.style = Paint.Style.STROKE
+
+        oval.set(
+            centerX - radius,
+            centerY - radius,
+            centerX + radius,
+            centerY + radius
+        )
+
+        canvas?.drawArc(oval, DEFAULT_ARC_ANGEL, DEFAULT_ARC_ANGEL, false, arcPaint)
+
+    }
+
+    private fun drawArrow(canvas: Canvas?) {
+
+        arrowPaint.color = arrowColor
+        arrowPaint.style = Paint.Style.FILL_AND_STROKE
+
+        arrowPath.reset()
+
+        arrowPath.addCircle(centerX, centerY, arrowRadius, Path.Direction.CW)
+
+        arrowPaint.strokeWidth = arrowWidth
+
+        arrowPath.moveTo(centerX, centerY)
+        arrowPath.lineTo(centerX - arrowLength, centerY)
+
+        if (indexFearGreedValue < 0) {
+            indexFearGreedValue = 0
+        } else if (indexFearGreedValue > 100) {
+            indexFearGreedValue = 100
+        }
+
+        val rotateArrowAngle = DEFAULT_ARC_ANGEL * (indexFearGreedValue / 100f)
+
+        arrowMatrix.reset()
+        arrowMatrix.setRotate(rotateArrowAngle, centerX, centerY)
+
+        arrowPath.transform(arrowMatrix)
+
+        canvas?.drawPath(arrowPath, arrowPaint)
+
+        fontPaint.textSize = fontSize
+        fontPaint.style = Paint.Style.STROKE
+        fontPaint.color = arrowTextColor
+        fontPaint.textAlign = Paint.Align.CENTER
+        val indexText = Integer.toString(indexFearGreedValue)
+        val textHeight = -(fontPaint.fontMetrics.ascent + fontPaint.fontMetrics.descent)
+        canvas?.drawText(indexText, centerX, centerY + textHeight / 2f, fontPaint)
+
+    }
+
+
+}
