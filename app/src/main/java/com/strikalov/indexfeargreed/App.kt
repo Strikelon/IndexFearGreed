5c460ec68025206298a03ff983b596ab0b540f62
Alexander Strikalov <strikalov.aleksandr@yandex.ru>
2019-06-25 16:29:57 +0300

    add Proguard and Crachlytics

--- app/src/main/java/com/strikalov/indexfeargreed/App.kt
+++ app/src/main/java/com/strikalov/indexfeargreed/App.kt
@@ -1,12 +1,15 @@
 package com.strikalov.indexfeargreed
 
 import android.app.Application
-import android.support.multidex.MultiDexApplication
 import com.strikalov.indexfeargreed.di.AppComponent
 import com.strikalov.indexfeargreed.di.AppModule
 import com.strikalov.indexfeargreed.di.DaggerAppComponent
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
-class App : MultiDexApplication() {
+
+
+class App : Application() {
 
     companion object {
 
@@ -18,6 +21,7 @@ class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         appComponent = generateAppComponent()
     }
 
